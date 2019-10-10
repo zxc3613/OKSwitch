148f5d295a00015c80b67e3e7f4ba60d7f52cfe7
Ilkang Oh <zxc3613@naver.com>
2019-10-10 16:18:10 +0900

    스프라이트 추가함
    -스위치 스프라이트 추가함.
    -씬에 스위치 형태 만듬.

---
+++ Assets/OKSwitch/Scripts/OKSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OKSwitch : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
